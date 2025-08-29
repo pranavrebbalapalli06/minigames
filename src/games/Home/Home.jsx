c5a512d13e5737139886f1bc21ffd31ecdbb0679
REBBALAPALLI PRANAV MARUTHI <pranavrebbalapalli>
2025-08-29 11:31:05 +0530

    Updated UI 6.0

--- src/games/Home/Home.jsx
+++ src/games/Home/Home.jsx
@@ -120,7 +120,7 @@ const Home = () => {
   return (
     <div className="relative min-h-screen w-full overflow-hidden">
       <RequireAuth />
-      <BackgroundBeamsWithCollision className="absolute inset-0 -z-10" />
+      <BackgroundBeamsWithCollision className="fixed inset-0 -z-10" />
       <TargetCursor spinDuration={2} hideDefaultCursor={true} />
 
       <div className="absolute top-4 right-4 z-10 bg-white/90 rounded-full px-4 py-2 shadow hover:shadow-xl transition">
